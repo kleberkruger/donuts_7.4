bd0fbedf958cb9be9e6b791628dd72d71f375454
Kleber Kruger <kleberkruger@gmail.com>
2021-07-13 13:14:25 -0400

    Correção: adicionado no método de seleção os modelos normal e readwirte para temporização da NVM

--- sniper/common/performance_model/nvm_perf_model.cc
+++ sniper/common/performance_model/nvm_perf_model.cc
@@ -1,6 +1,8 @@
 #include "simulator.h"
 #include "nvm_perf_model.h"
 #include "nvm_perf_model_constant.h"
+#include "nvm_perf_model_readwrite.h"
+#include "nvm_perf_model_normal.h"
 #include "config.hpp"
 
 NvmPerfModel* NvmPerfModel::createNvmPerfModel(core_id_t core_id, UInt32 cache_block_size)
@@ -10,6 +12,14 @@ NvmPerfModel* NvmPerfModel::createNvmPerfModel(core_id_t core_id, UInt32 cache_b
    if (type == "constant")
    {
       return new NvmPerfModelConstant(core_id, cache_block_size);
+   }
+   else if (type == "readwrite")
+   {
+      return new NvmPerfModelReadWrite(core_id, cache_block_size);
+   }
+   else if (type == "normal")
+   {
+      return new NvmPerfModelNormal(core_id, cache_block_size);
    }
    else
    {
